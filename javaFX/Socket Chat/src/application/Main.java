a4e2ccbe0391efd95ac2646c398085e3202db6f2
codekidX <ckrackerx15@gmail.com>
2016-08-10 22:05:26 +0530

    javaFX: commented everything

--- javaFX/Socket Chat/src/application/Main.java
+++ javaFX/Socket Chat/src/application/Main.java
@@ -13,16 +13,24 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			StartViewController startViewController = new StartViewController();
+			// fxml file is for UI/layout of the screen
 			 FXMLLoader loader = new FXMLLoader();
+			 // StartView.fxml is for the starting screen that you see with drawer icon etc ..
 	            loader.setLocation(Main.class.getResource("StartView.fxml"));
 	            
+	            // layout should have a controller which controls button click and all other various events
 	            loader.setController(startViewController);
+	            // parent of all UI components
 	            rootLayout = (AnchorPane) loader.load();
 	            
 	            
 	            
 	        // Show the scene containing the root layout.
 	        Scene scene = new Scene(rootLayout);
+	        // Inside Stage --> Scene --> Components
+	        // model-view-controller pattern
+	        
+	        //set title (create Socket Chat | Your name) with server code and let this be client. FOR TESTING
 			primaryStage.setTitle("Socket Chat | Ashish");
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -32,6 +40,7 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		// for now it launcher the application file with args when u press RUN
 		launch(args);
 	}
 }
--- javaFX/Socket Chat/src/application/StartViewController.java
+++ javaFX/Socket Chat/src/application/StartViewController.java
@@ -35,7 +35,11 @@ public class StartViewController implements Initializable {
 		// TODO Auto-generated method stub
 		
 		try {
+			// Same way you loaded StartView.fxml in Main file your are loading NavigationView.fxml in drawer
+			
+			// this time the parent/stage is VBox(vertical box) not AnchorPane
 			VBox vbox = FXMLLoader.load(getClass().getResource("NavigationView.fxml"));
+			// set that view to drawer
 			navDrawer.setSidePane(vbox);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
@@ -45,12 +49,15 @@ public class StartViewController implements Initializable {
 		// ham icon related code
 		HamburgerBackArrowBasicTransition hamburgerBackArrowBasicTransition = new HamburgerBackArrowBasicTransition(hamButton);
 		
+		// at start don't show it OR no transition is to be made
 		hamburgerBackArrowBasicTransition.setRate(-1);
 		
 		hamButton.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+			// set rate of transition speed
 			hamburgerBackArrowBasicTransition.setRate(hamburgerBackArrowBasicTransition.getRate() * -1);
 			hamburgerBackArrowBasicTransition.play();
 			
+			// this code is obvious -  hamburger used as toggle
 			if(navDrawer.isShown()) {
 				navDrawer.close();
 			} else {
